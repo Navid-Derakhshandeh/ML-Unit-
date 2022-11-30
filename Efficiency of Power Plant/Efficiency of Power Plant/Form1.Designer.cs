35e3645be3b616aedb9b5d485f30fbceeaa86760
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2022-12-01 02:19:53 +0330

    ML Unit Developed for CIP Project

---
+++ Efficiency of Power Plant/Efficiency of Power Plant/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Efficiency_of_Power_Plant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtW = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gas :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Water :";
+            // 
+            // txtG
+            // 
+            this.txtG.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtG.Location = new System.Drawing.Point(83, 27);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(100, 26);
+            this.txtG.TabIndex = 2;
+            // 
+            // txtW
+            // 
+            this.txtW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtW.Location = new System.Drawing.Point(83, 67);
+            this.txtW.Name = "txtW";
+            this.txtW.Size = new System.Drawing.Size(100, 26);
+            this.txtW.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnRun.Location = new System.Drawing.Point(58, 117);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(76, 38);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "RUN";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(12, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result:";
+            // 
+            // txtRes
+            // 
+            this.txtRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtRes.Location = new System.Drawing.Point(83, 177);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(100, 26);
+            this.txtRes.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(12, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Condition:";
+            // 
+            // LS
+            // 
+            this.LS.AutoSize = true;
+            this.LS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LS.Location = new System.Drawing.Point(98, 221);
+            this.LS.Name = "LS";
+            this.LS.Size = new System.Drawing.Size(20, 20);
+            this.LS.TabIndex = 9;
+            this.LS.Text = "S";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 260);
+            this.Controls.Add(this.LS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtW);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtW;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LS;
+    }
+}
+
---
+++ Efficiency of Power Plant/Efficiency of Power Plant/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ML;
+using Efficiency_of_Power_PlantML.Model;
+
+namespace Efficiency_of_Power_Plant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MLContext mlContext = new MLContext();
+                ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+                var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+                var input = new ModelInput();
+
+                int Ga = Int32.Parse(txtG.Text);
+
+
+                int Wa = Int32.Parse(txtW.Text);
+
+                input.Gas = Ga;
+                input.Water = Wa;
+
+                ModelOutput result = predEngine.Predict(input);
+
+                int b = (int)Math.Round(result.Score);
+                txtRes.Text = b.ToString();
+                if (b >= 100)
+                {
+                    string c = "Your System is Efficient";
+                    LS.Text = c;
+                }
+                else if (b <= 99)
+                {
+                    string c1 = "your system is not Efficient";
+
+                    LS.Text = c1;
+                }
+            }
+            catch(Exception ex)
+            {
+                LS.Text = ex.Message;
+            }
+            
+        }
+    }
+}
